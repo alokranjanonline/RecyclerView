52bec740ede8650a439db549ed8bf34efddd6d7e
aloksport <alokranjan999@gmail.com>
2023-12-05 12:32:45 +0530

    Interestitial Ad working without creating function
    Interestitial Ad working without creating function date 05-Dec-2023

--- app/src/main/java/com/example/recyclerview/CustomAdapter.kt
+++ app/src/main/java/com/example/recyclerview/CustomAdapter.kt
@@ -1,18 +1,27 @@
 package com.example.recyclerview
 
+import android.app.Activity
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 class CustomAdapter(private val mList: List<ItemsViewModel>, var context: Context) :
     RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
-
+    private var mInterstitialAd: InterstitialAd? = null
     // create new views
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         // inflates the card_view_design view
@@ -34,10 +43,61 @@ class CustomAdapter(private val mList: List<ItemsViewModel>, var context: Contex
         // sets the text to the textview from our itemHolder class
         holder.textView.text = ItemsViewModel.text
         holder.itemView.setOnClickListener(){
+            var adRequest = AdRequest.Builder().build()
+            InterstitialAd.load(context,"ca-app-pub-3940256099942544/1033173712", adRequest, object : InterstitialAdLoadCallback() {
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    mInterstitialAd = null
+                    holder.textView.text="Not Loaded"
+                }
+
+                override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                    mInterstitialAd = interstitialAd
+                    holder.textView.text="Ad Loaded"
+                }
+            })
+            mInterstitialAd?.fullScreenContentCallback = object: FullScreenContentCallback() {
+                override fun onAdClicked() {
+                    // Called when a click is recorded for an ad.
+                    holder.textView.text="Ad was clicked."
+                    Log.d(ContentValues.TAG, "Ad was clicked.")
+                }
+
+                override fun onAdDismissedFullScreenContent() {
+                    // Called when ad is dismissed.
+                    Log.d(ContentValues.TAG, "Ad dismissed fullscreen content.")
+                    holder.textView.text="Ad dismissed fullscreen content."
+                    mInterstitialAd = null
+                    val intent = Intent(context, MainActivity2::class.java)
+                    intent.putExtra("message_key", ItemsViewModel.text)
+                    context.startActivity(intent)
+                }
+
+                /*override fun onAdFailedToShowFullScreenContent(adError: AdError?) {
+                    // Called when ad fails to show.
+                    textActivity.text="Ad failed to show fullscreen content."
+                    mInterstitialAd = null
+                }*/
+
+                override fun onAdImpression() {
+                    // Called when an impression is recorded for an ad.
+                    Log.d(ContentValues.TAG, "Ad recorded an impression.")
+                    holder.textView.text="Ad recorded an impression."
+                }
+
+                override fun onAdShowedFullScreenContent() {
+                    // Called when ad is shown.
+                    holder.textView.text="Ad showed fullscreen content."
+                    Log.d(ContentValues.TAG, "Ad showed fullscreen content.")
+                }
+            }
+            if (mInterstitialAd != null) {
+                mInterstitialAd?.show( context as Activity )
+
+            } else {
+                holder.textView.text="The interstitial ad wasn't ready yet."
+            }
             Toast.makeText(context, ItemsViewModel.text, Toast.LENGTH_SHORT).show()
-            val intent = Intent(context, MainActivity2::class.java)
-            intent.putExtra("message_key", ItemsViewModel.text)
-            context.startActivity(intent)
+
         }
 
     }
