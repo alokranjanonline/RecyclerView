049ef10b2bfc91269128789cc4f80a8bb044efbd
aloksport <alokranjan999@gmail.com>
2023-12-13 15:10:00 +0530

    13-Dec-2023

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -13,12 +13,27 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.example.recyclerview.ui.theme.RecyclerViewTheme
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mViewPager: ViewPager2 =findViewById(R.id.my_view_pager)
+        val mTabLayout: TabLayout =findViewById(R.id.my_tab_layout)
+
+        mViewPager.adapter= MyVPAdapter(this)
+        TabLayoutMediator(mTabLayout,mViewPager){
+                tab,position->
+            when(position){
+                0->tab.text="Profile"
+                1->tab.text="Chats"
+            }
+        }.attach()
 
         // getting the recyclerview by its id
         val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
